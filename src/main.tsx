328633ffea977bc4d1515e5ccca5caa724bef4b4
seraph0153 <seraph0153@users.noreply.github.com>
2026-02-13 13:51:01 +0900

    fix: Restore imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import ErrorBoundary from './components/common/ErrorBoundary';
 
 console.log('Main.tsx is running');
